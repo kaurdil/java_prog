94dd5d6ebbce441c43b5625bd63b467f98d25780
kaurdil <50011122+kaurdil@users.noreply.github.com>
2020-01-05 13:01:19 +0530

    Add files via upload

---
+++ Staticar.java
@@ -0,0 +1,15 @@
+class Staticar
+     {
+         public static void main(String[] args) 
+           {
+                    int ar[];
+                    ar=new int[5];
+                    ar[0]=10;
+                    ar[1]=20;
+                    ar[2]=30;
+                    ar[3]=40;
+                    ar[4]=50;
+                for(int i=0; i<ar.length; i++)
+                    System.out.println("element at:"+i+"is"+ar[i]);
+            }
+     } 
